559fce6ceab0d87203829ac2263ea8f854adf3dc
Walter <walter.salas@me.com>
2020-07-04 13:42:14 -0500

    styles, bootstrap, notes

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,34 +1,41 @@
-import React from 'react'
-import confLogo from '../images/badge-header.svg'
+import React from 'react';
+import './styles/Badge.css';
+import confLogo from '../images/badge-header.svg';
 
 class Badge extends React.Component {
 
     render () {
-        return <div>
-            <div>
+        return (
+        <div className="Badge" >
+
+            <div className="Badge__header">
                 <img src={confLogo} alt="Logo"/>
             </div>
-            <div>
-                <img src="https://secure.gravatar.com/avatar/e163c2bf9f1a86e2567e61cbb47e8a21" alt="Avatar"/>
+
+            <div className="Badge__section-name">
+                <img 
+                className="Badge__avatar"
+                src="https://secure.gravatar.com/avatar/e163c2bf9f1a86e2567e61cbb47e8a21" 
+                alt="Avatar"/>
                 <h1>Walter <br/> Salas</h1>
             </div>
 
-            <div>
-                <p>
+            <div className="Badge__section-info">
+                <h3>
                     Frontend Develoer
-                </p>
-                <p>
+                </h3>
+                <div>
                     @soywaltersalas
-                </p>
+                </div>
             </div>
 
-            <div>
+            <div className="Badge__footer">
                 #platziconf
             </div>
 
-        </div>;
+        </div>
+        );
     }
-
 }
 
 export default Badge;
